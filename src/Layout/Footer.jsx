1e15b732d08dd193936a16e953f5d8ae83df302b
Aitziber <aitzibergonzalez14@gmail.com>
2025-12-02 13:48:17 +0100

    Added the login and the Footer part

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,77 @@
+
+
+function Footer(){
+    return(
+        <div>
+            <div class="container my-5">
+                <footer class="text-center text-lg-start text-white" style="background-color: #45526e">
+                     <div class="container p-4 pb-0">
+                        <section class="">
+                            <div class="row">
+                            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                                <h6 class="text-uppercase mb-4 font-weight-bold"> TripiFy</h6>
+                                <p>
+                                    Here you can use rows and columns to organize your footer
+                                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                    elit.
+                                </p>
+                            </div>
+
+                            <hr class="w-100 clearfix d-md-none" />
+                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                                <p>
+                                    <a class="text-white">Travels</a>
+                                </p>
+                                <p>
+                                    <a class="text-white">Hotels</a>
+                                </p>
+                                <p>
+                                    <a class="text-white">Tickets</a>
+                                </p>
+                            </div>
+
+
+                            <hr class="w-100 clearfix d-md-none" />
+                                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                                    <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                                    <p><i class="fas fa-envelope mr-3"></i> infoTripiFy@gmail.com</p>
+                                    <p><i class="fas fa-phone mr-3"></i>+34 200749518</p>
+                                </div>
+                            </div>
+                        </section>
+
+                        <hr class="my-3"></hr>
+                        <section>
+                            <div class="row d-flex align-items-center">
+                                <div class="col-md-7 col-lg-8 text-center text-md-start">
+                                    <div class="p-3">
+                                        © 2025 Copyright
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                                <a class="btn btn-outline-light btn-floating m-1 text-white" role="button">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+
+                                <a class="btn btn-outline-light btn-floating m-1 text-white" role="button">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+
+                                <a class="btn btn-outline-light btn-floating m-1 text-white" role="button">
+                                    <i class="fab fa-google"></i>
+                                </a>
+
+                                <a class="btn btn-outline-light btn-floating m-1 text-white" role="button">
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                            </div>
+                        </section>
+                     </div>
+                </footer>
+            </div>
+        </div>
+    );
+}
