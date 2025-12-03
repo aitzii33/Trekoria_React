f999b6bf597c2e96d9a38e9f954bfa8906bc7c4a
Aitziber <aitzibergonzalez14@gmail.com>
2025-12-03 09:42:54 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logIn from '../src/Layout/LogIn'
 import './App.css'
-import Head from './Paginas/Head_Foot/Header';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>
-    <Head />
-   </div>
-     
+    <>
+      <logIn></logIn>
+    </>
   )
 }
 
