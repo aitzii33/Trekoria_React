8c0b3310646c4de81ae616a9c9b021421efa8b33
umama <ushahzadi24dw@ikzubirimanteo.com>
2025-12-04 13:32:37 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import logIn from './Layout/LogIn';
 import './App.css'
 import Header from './Components/Header';
+import Landing from './Layout/Landing';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Landing/>
     </>
   )
 }
