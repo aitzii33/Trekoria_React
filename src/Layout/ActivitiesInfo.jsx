72d642696553cc1d36f59e627ccd1c57cfbc2b5d
umama <ushahzadi24dw@ikzubirimanteo.com>
2026-01-13 09:41:58 +0100

    Bring all missing files and updates from branch umama

--- src/Layout/ActivitiesInfo.jsx
+++ src/Layout/ActivitiesInfo.jsx
@@ -1,4 +1,4 @@
-import { Row, Col } from "reactstrap"
+import { Container, Row, Col } from "reactstrap"
 import { useState } from "react"
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
@@ -11,8 +11,6 @@ import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 
 import BoatImg from "../assets/img/Boat.avif"
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../assets/CSS/Activity_details.css";
 
 
 
@@ -28,26 +26,32 @@ function ActivitiesInfo()
     });
 
 
+    const [Activity] = useState({
+        image: BoatImg,
+        name: "Kayak in the river",
+        description: "Activity giuded for all the ages and levels",
+        price: "25€",
+    });
+
+
     return(
-        <>
+        <Container fluid className="px-1 px-sm-5 mx-auto mt-4">
             <Header/>
                 <Row>
-<<<<<<< HEAD
                     <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-=======
-                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" >
->>>>>>> 057fefd (modify css of the activity details)
                         <div className="carousel-inner">
                             <div className="carousel-item active">
+                                <img src={Activity.image} className="d-block w-100" alt="activity image"/>
                                 <img src={Activity.image} className="d-block w-100" alt="activity image"/>
                             </div>
 
                             <div className="carousel-item">
                                 <img src={Activity.image} className="d-block w-100" alt="activity image"/>
+                                <img src={Activity.image} className="d-block w-100" alt="activity image"/>
                             </div>
                             
                             <div className="carousel-item">
+                                <img src={Activity.image} className="d-block w-100" alt="activity image"/>
                                 <img src={Activity.image} className="d-block w-100" alt="activity image"/>
                             </div>
                         </div>
@@ -64,7 +68,6 @@ function ActivitiesInfo()
                     </div>
                 </Row>
 
-<<<<<<< HEAD
                 <Row>
                     <Col md={8} className="activity-info d-flex flex-column">
                         <h2>{activity.name}</h2>
@@ -76,68 +79,31 @@ function ActivitiesInfo()
                     </Col>
 
                     <Col>
-=======
-                <Row className="justify-content-center mt-4">
-                    <Col md={8} className="activity-info">
-                        <h2>Kayak in the river</h2>
-                        <p className="description">Activity giuded for all the ages and levels</p>
-
-                        <div className="activity-form">
->>>>>>> 057fefd (modify css of the activity details)
                         <div className="col-auto">
-                            <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)}
-                            dateFormat="dd-MM-yyyy"
-                            highlightDates={[new Date()]}
-                            placeholderText="Selecciona una fecha"
-                            className="form-control"
-                            name="date"
-                            />
+                            <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} dateFormat="dd-MM-yyyy" highlightDates={[new Date()]} placeholderText="Selecciona una fecha" className="form-control" name="date" />
                         </div>
 
-                        <div className="col-auto dropdown">
-                            <button
-                            type="button"
-                            className="btn dropdown-toggle"
-                            data-bs-toggle="dropdown"
-                            >
-                            HOURS
-                            </button>
+                        <div className="col-auto dropdown">           
+                            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown"> HOURS </button>
                             <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Link 1</a></li>
-                            <li><a className="dropdown-item" href="#">Link 2</a></li>
-                            <li><a className="dropdown-item" href="#">Link 3</a></li>
+                                <li><a className="dropdown-item" href="#">Link 1</a></li>
+                                <li><a className="dropdown-item" href="#">Link 2</a></li>
+                                <li><a className="dropdown-item" href="#">Link 3</a></li>
                             </ul>
                         </div>
 
-                        <div className="col-auto dropdown">
-                            <button
-                            type="button"
-                            className="btn dropdown-toggle"
-                            data-bs-toggle="dropdown"
-                            >
-                            People
-                            </button>
+                        <div className="col-auto dropdown">           
+                            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown"> People </button>
                             <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Link 1</a></li>
-                            <li><a className="dropdown-item" href="#">Link 2</a></li>
-                            <li><a className="dropdown-item" href="#">Link 3</a></li>
+                                <li><a className="dropdown-item" href="#">Link 1</a></li>
+                                <li><a className="dropdown-item" href="#">Link 2</a></li>
+                                <li><a className="dropdown-item" href="#">Link 3</a></li>
                             </ul>
-                        </div>
-                        </div>
-
-                        <div className="activity-actions">
-                        <span className="price">25€</span>
-                        <button className="btn-reserve">Reserve</button>
                         </div>
                     </Col>
                 </Row>
             <Footer/>
-<<<<<<< HEAD
         </Container>
-        </Container>
-=======
-        </>
->>>>>>> 057fefd (modify css of the activity details)
     );
 }
 
