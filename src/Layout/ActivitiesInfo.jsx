ed67a2d1d21809c40d2385016792311d1362fb89
Aitziber <aitzibergonzalez14@gmail.com>
2026-01-13 09:34:22 +0100

    modify css of the activity details

--- src/Layout/ActivitiesInfo.jsx
+++ src/Layout/ActivitiesInfo.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "reactstrap"
+import { Row, Col } from "reactstrap"
 import { useState } from "react"
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
@@ -11,8 +11,8 @@ import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 
 import BoatImg from "../assets/img/Boat.avif"
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/CSS/Activity_details.css";
 
 function ActivitiesInfo()
 {
@@ -27,11 +27,10 @@ function ActivitiesInfo()
 
 
     return(
-        <Container fluid className="px-1 px-sm-5 mx-auto mt-4">
+        <>
             <Header/>
                 <Row>
-                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" >
                         <div className="carousel-inner">
                             <div className="carousel-item active">
                                 <img src={Activity.image} className="d-block w-100" alt="activity image"/>
@@ -58,43 +57,61 @@ function ActivitiesInfo()
                     </div>
                 </Row>
 
-                <Row>
-                    <Col md={8} className="activity-info d-flex flex-column">
-                        <h2>{activity.name}</h2>
-                        <p className="description">{activity.description}</p>
+                <Row className="justify-content-center mt-4">
+                    <Col md={8} className="activity-info">
+                        <h2>Kayak in the river</h2>
+                        <p className="description">Activity giuded for all the ages and levels</p>
 
-                        <div className="activity-actions mt-auto">
-                            <span className="price">{activity.price}</span>
-                        </div>
-                    </Col>
-
-                    <Col>
+                        <div className="activity-form">
                         <div className="col-auto">
-                            <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} dateFormat="dd-MM-yyyy" highlightDates={[new Date()]} placeholderText="Selecciona una fecha" className="form-control" name="date" />
+                            <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)}
+                            dateFormat="dd-MM-yyyy"
+                            highlightDates={[new Date()]}
+                            placeholderText="Selecciona una fecha"
+                            className="form-control"
+                            name="date"
+                            />
                         </div>
 
-                        <div className="col-auto dropdown">           
-                            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown"> HOURS </button>
+                        <div className="col-auto dropdown">
+                            <button
+                            type="button"
+                            className="btn dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            >
+                            HOURS
+                            </button>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Link 1</a></li>
-                                <li><a className="dropdown-item" href="#">Link 2</a></li>
-                                <li><a className="dropdown-item" href="#">Link 3</a></li>
+                            <li><a className="dropdown-item" href="#">Link 1</a></li>
+                            <li><a className="dropdown-item" href="#">Link 2</a></li>
+                            <li><a className="dropdown-item" href="#">Link 3</a></li>
                             </ul>
                         </div>
 
-                        <div className="col-auto dropdown">           
-                            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown"> People </button>
+                        <div className="col-auto dropdown">
+                            <button
+                            type="button"
+                            className="btn dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            >
+                            People
+                            </button>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Link 1</a></li>
-                                <li><a className="dropdown-item" href="#">Link 2</a></li>
-                                <li><a className="dropdown-item" href="#">Link 3</a></li>
+                            <li><a className="dropdown-item" href="#">Link 1</a></li>
+                            <li><a className="dropdown-item" href="#">Link 2</a></li>
+                            <li><a className="dropdown-item" href="#">Link 3</a></li>
                             </ul>
+                        </div>
+                        </div>
+
+                        <div className="activity-actions">
+                        <span className="price">25€</span>
+                        <button className="btn-reserve">Reserve</button>
                         </div>
                     </Col>
                 </Row>
             <Footer/>
-        </Container>
-        </Container>
+        </>
     );
 }
 
